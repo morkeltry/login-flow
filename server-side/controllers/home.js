78d3e4d1fad2c747365b857ecc9810f775cadbf9
Mork <github@morkeltry.33mail.com>
2019-08-22 19:26:14 +0100

    index.html redirects to login.html if no 'loggedInUser' cookie is present

--- server-side/controllers/home.js
+++ server-side/controllers/home.js
@@ -1,5 +1,7 @@
-const home = (req, res) =>
- { res.type('text/html'); res.status(200); res.send('<p>Express default / route - not using static files for this route.</p>');
+const home = (req, res) => {
+ res.type('text/html');
+ res.status(200);
+ res.send('<p>Express default / route - not using static files for this route.</p>');
   // res.render('home', { params : {} });
 }
 
--- server-side/controllers/index.js
+++ server-side/controllers/index.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const home = require('./home');
 
 router.get('/', home);
+router.get('/welcome', home);
 
 module.exports = router;
